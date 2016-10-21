479059e6be67d158cd8dcc8b7d1048ee50df9a9e
Stefan Marr <git@stefan-marr.de>
2016-10-22 01:30:32 +0200

    Use AWFY ref id
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PARAMS=("-d" "--without-nice" "--commit-id=\"$CI_BUILD_REF\"" "--environment=Infinity Ubuntu" "--project=AWFY" "--branch=\"$CI_BUILD_REF_NAME\"")
+AWFY_REF=`(cd awfy && git rev-parse HEAD)`
+PARAMS=("-d" "--without-nice" "--commit-id=\"$AWFY_REF\"" "--environment=Infinity Ubuntu" "--project=AWFY" "--branch=\"$CI_BUILD_REF_NAME\"")
 rebench "${PARAMS[@]}" codespeed.conf ruby-others vm:MRI23
 exit 0
 # rebench -d --without-nice codespeed.conf steady-java
