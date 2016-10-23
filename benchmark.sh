e3dceed8f834a6df1e78e765ce552129ff4d3d80
Stefan Marr <git@stefan-marr.de>
2016-10-23 12:45:11 +0200

    Run Graal, JRuby, and TruffleSOM
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -3,11 +3,10 @@
 CI_BUILD_REF=2cd905bd67a061705eaf8098832f49886d5c9de2
 PARAMS=("-d" "--without-nice" "--commit-id=\"$CI_BUILD_REF\"" "--environment=Infinity Ubuntu" "--project=AWFY" "--branch=\"$CI_BUILD_REF_NAME\"")
 
-# rebench "${PARAMS[@]}" codespeed.conf all vm:GraalBasic vm:GraalC2
-# rebench "${PARAMS[@]}" codespeed.conf all vm:JRubyGraal vm:JRubyTruffle
-#
-# rebench "${PARAMS[@]}" codespeed.conf all vm:TruffleSOM vm:TruffleSOM-TOM
-rebench "${PARAMS[@]}" codespeed.conf all vm:SOMns
+rebench "${PARAMS[@]}" codespeed.conf all vm:GraalBasic vm:GraalC2
+rebench "${PARAMS[@]}" codespeed.conf all vm:JRubyGraal vm:JRubyTruffle
+rebench "${PARAMS[@]}" codespeed.conf all vm:TruffleSOM vm:TruffleSOM-TOM
+# rebench "${PARAMS[@]}" codespeed.conf all vm:SOMns
 # rebench "${PARAMS[@]}" codespeed.conf all
 
 # rebench -d --without-nice codespeed.conf steady-java
