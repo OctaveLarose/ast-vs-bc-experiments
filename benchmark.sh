2f4781e98a48dab4af322375e37f8187b608b618
Stefan Marr <git@stefan-marr.de>
2016-12-03 20:43:12 +0100

    Update Java to 8b111, Crystal 0.19.4 to 0.20.0, Node 7.0 to 7.2
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,11 @@
 # CI_BUILD_REF=2cd905bd67a061705eaf8098832f49886d5c9de2
 PARAMS=("-d" "--without-nice" "--commit-id=$CI_BUILD_REF" "--environment=Infinity Ubuntu" "--project=AWFY" "--branch=$CI_BUILD_REF_NAME")
 
+rebench "${PARAMS[@]}" codespeed.conf all vm:Node
+rebench "${PARAMS[@]}" codespeed.conf all vm:NodeTurboFan
+rebench "${PARAMS[@]}" codespeed.conf steady-java
+rebench "${PARAMS[@]}" codespeed.conf steady-crystal
+
 # rebench "${PARAMS[@]}" codespeed.conf all vm:GraalBasic vm:GraalC2
 # rebench "${PARAMS[@]}" codespeed.conf all vm:JRubyGraal vm:JRubyTruffle
 # rebench "${PARAMS[@]}" codespeed.conf all vm:TruffleSOM vm:TruffleSOM-TOM
@@ -10,13 +15,9 @@ PARAMS=("-d" "--without-nice" "--commit-id=$CI_BUILD_REF" "--environment=Infinit
 # rebench "${PARAMS[@]}" codespeed.conf all
 # rebench "${PARAMS[@]}" codespeed.conf all vm:Crystal
 # rebench "${PARAMS[@]}" codespeed.conf all vm:Java8U66
-rebench "${PARAMS[@]}" codespeed.conf all vm:Node
 # rebench "${PARAMS[@]}" codespeed.conf all vm:JavaInt
 # rebench "${PARAMS[@]}" codespeed.conf all vm:SOMnsInt
-rebench "${PARAMS[@]}" codespeed.conf all vm:NodeTurboFan
 
-# rebench -d --without-nice codespeed.conf steady-java
-# rebench -d --without-nice codespeed.conf steady-crystal
 # rebench -d --without-nice codespeed.conf steady-js
 # rebench -d --without-nice codespeed.conf ruby-others
 # rebench -d --without-nice codespeed.conf steady-ruby
