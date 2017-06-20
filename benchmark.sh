f8bc7b8cb21fc1000c9ea651a6b3c2f2a37e8f2c
Stefan Marr <git@stefan-marr.de>
2017-06-20 22:51:42 +0200

    Update Graal and run SOMns and Graal benchmarks
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -13,7 +13,7 @@ git submodule update --recursive
 
 # rebench "${PARAMS[@]}" codespeed.conf lua
 
-# rebench "${PARAMS[@]}" codespeed.conf all vm:GraalBasic vm:GraalC2
+rebench "${PARAMS[@]}" codespeed.conf all vm:GraalBasic vm:GraalC2 vm:SOMns
 # rebench "${PARAMS[@]}" codespeed.conf all vm:JRubyGraal vm:JRubyTruffle
 # rebench "${PARAMS[@]}" codespeed.conf all vm:TruffleSOM vm:TruffleSOM-TOM
 # rebench "${PARAMS[@]}" codespeed.conf all vm:SOMns
@@ -24,7 +24,7 @@ git submodule update --recursive
 # rebench "${PARAMS[@]}" codespeed.conf all vm:SOMns
 
 # rebench "${PARAMS[@]}" codespeed.conf all vm:SOMpp
-rebench "${PARAMS[@]}" codespeed.conf all vm:Node vm:NodeTurboFan
+# rebench "${PARAMS[@]}" codespeed.conf all vm:Node vm:NodeTurboFan
 # rebench "${PARAMS[@]}" codespeed.conf all vm:Java8U66  vm:JavaInt
 
 
