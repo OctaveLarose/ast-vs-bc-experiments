16d238a11bf63ea03ce352e9e743115f0eca20a0
Stefan Marr <git@stefan-marr.de>
2017-07-01 23:02:12 +0200

    Run new SOMns version
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -21,10 +21,10 @@ git submodule update --recursive
 # rebench -f "${PARAMS[@]}" codespeed.conf all
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:Crystal vm:Node vm:NodeTurboFan
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:RSqueak
-# rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMnsInt
-# rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMns
+rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMnsInt
+rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMns
 
-rebench -f "${PARAMS[@]}" codespeed.conf all vm:TruffleSOM-TOM
+# rebench -f "${PARAMS[@]}" codespeed.conf all vm:TruffleSOM-TOM
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMppOMR
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMpp
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:Node vm:NodeTurboFan
