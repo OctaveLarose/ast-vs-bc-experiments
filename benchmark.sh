ea065a0cc5b3b338c0ee1e56fe535e3b5190c9af
Stefan Marr <git@stefan-marr.de>
2017-11-07 09:17:17 +0000

    Update GraalVM 0.24 to 0.29
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -18,9 +18,11 @@ git submodule update --recursive
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:TruffleSOM vm:TruffleSOM-TOM
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMns
 
+rebench -f "${PARAMS[@]}" codespeed.conf all vm:GraalEnterprise vm:GraalJS
+
 # vm:Crystal 
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:Node
-rebench -f "${PARAMS[@]}" codespeed.conf all vm:RSqueak vm:Topaz
+# rebench -f "${PARAMS[@]}" codespeed.conf all vm:RSqueak vm:Topaz
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMnsInt
 # rebench -f "${PARAMS[@]}" codespeed.conf all vm:SOMns
 
