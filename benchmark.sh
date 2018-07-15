754c1e4155bf80067aeab15267d9133ac0607c7b
Stefan Marr <git@stefan-marr.de>
2018-07-15 22:51:51 +0100

    Do a quick run
    - no debug mode anymore
    - compress results
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #AWFY_REF=`(cd awfy && git rev-parse HEAD)`
 # CI_BUILD_REF=2cd905bd67a061705eaf8098832f49886d5c9de2
-PARAMS=("-d" "--without-nice" "--commit-id=$CI_BUILD_REF" "--environment=Infinity Ubuntu" "--project=AWFY" "--branch=$CI_BUILD_REF_NAME")
+PARAMS=("--without-nice" "--commit-id=$CI_BUILD_REF" "--environment=Infinity Ubuntu" "--project=AWFY" "--branch=$CI_BUILD_REF_NAME")
 git submodule update --recursive
 
 # (cd awfy && ./implementations/setup.sh)
@@ -53,7 +53,8 @@ VMS=(
   # vm:Lua53
 )
 
-rebench -f "${PARAMS[@]}" codespeed.conf all "${VMS[@]}"
+#rebench -f "${PARAMS[@]}" codespeed.conf all "${VMS[@]}"
+rebench -q -f "${PARAMS[@]}" codespeed.conf all
 
 
 ## Archive Results
@@ -69,6 +70,7 @@ TARGET_PATH=$DATA_ROOT/$NUM_PREV-$REV
 LATEST=$DATA_ROOT/latest
 
 mkdir -p $TARGET_PATH
-cp benchmark.data $TARGET_PATH/
+bzip2 benchmark.data
+cp benchmark.data.bz2 $TARGET_PATH/
 rm $LATEST
 ln -s $TARGET_PATH $LATEST
