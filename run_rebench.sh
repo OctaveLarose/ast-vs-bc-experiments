3ad3ef7b064b3705a8dd78e79ae9c8afd932875e
Octave Larose <octave.larose@hotmail.fr>
2023-07-13 16:01:58 +0100

    uncommenting optimisations experiment + attempting to improve run_rebench.sh script

--- run_rebench.sh
+++ run_rebench.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-rebench -f --without-building -c ast-vs-bc.conf ${1:-all-normal}
+# if no arguments, run the very fast minimal case
+if [ "$#" -eq  "0" ]
+then
+     rebench --it 1 --in 1 -f --without-building -c ast-vs-bc.conf overview
+else
+     rebench -f --without-building -c ast-vs-bc.conf $1
+fi
