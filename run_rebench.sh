dbfa513fd88ad4fd9be0b814be2ad91be8ff7b95
Octave Larose <octave.larose@hotmail.fr>
2023-07-14 17:48:46 +0100

    max rss for python in config

--- run_rebench.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# if no arguments, run the very fast minimal case
-if [ "$#" -eq  "0" ]
-then
-     rebench --it 5 --in 1 -f --without-building -c ast-vs-bc.conf minimal
-else
-     rebench -f --without-building -c ast-vs-bc.conf $1
-fi
