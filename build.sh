f65db721da6e2e5170ba76c527e6c33663f5aa7f
Stefan Marr <git@stefan-marr.de>
2023-07-15 09:14:49 +0100

    Make sure we use -9 level compression

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 echo "===Building the ast-vs-bc Docker image and exporting it to ast-vs-bc.tar.gz"
 
 docker build . -f Dockerfile -t ast-vs-bc
-docker save ast-vs-bc | gzip > ast-vs-bc.tar.gz
+docker save ast-vs-bc | gzip -9 > ../ast-vs-bc.tar.gz
 
 echo "===Image should now be available in ast-vs-bc.tar.gz"
